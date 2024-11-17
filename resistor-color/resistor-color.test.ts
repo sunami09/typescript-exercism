136f35c57f26dfd59e288aef3e454227ed041ff5
sunami09 <sunamidasgupta@gmail.com>
2024-11-16 16:25:53 -0800

    Ex 3 completed

---
+++ resistor-color/resistor-color.test.ts
@@ -0,0 +1,31 @@
+import { describe, xdescribe, it, expect, xit } from '@jest/globals'
+import { colorCode, COLORS } from './resistor-color.ts'
+describe('Colors', () => {
+  it('returns all colors', () => {
+    expect(COLORS).toEqual([
+      'black',
+      'brown',
+      'red',
+      'orange',
+      'yellow',
+      'green',
+      'blue',
+      'violet',
+      'grey',
+      'white',
+    ])
+  })
+})
+describe('color code', () => {
+  it('Black', () => {
+    expect(colorCode('black')).toEqual(0)
+  })
+
+  it('White', () => {
+    expect(colorCode('white')).toEqual(9)
+  })
+
+  it('Orange', () => {
+    expect(colorCode('orange')).toEqual(3)
+  })
+})
